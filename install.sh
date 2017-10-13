3ff189973219f5fd1149c09bcc82e0501c00c8de
Plotnikov Anton <plotnikovanton@gmail.com>
2017-10-13 17:59:09 +0300

    Handle only diktor layout

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
-OUTPUT_PATH=/usr/share/X11/xkb/symbols/prog
+OUTPUT_PATH=/usr/share/X11/xkb/symbols/ru
+START='START DIKTOR LAYOUT'
+END='END DIKTOR LAYOUT'
 
-if [ -s $OUTPUT_PATH ] && [ "$1" != '--force' ]; then
-    echo "File in $OUTPUT_PATH exists!"
+if grep "$START" $OUTPUT_PATH && grep "$END" $OUTPUT_PATH
+then
+	echo "Found previous instalation, replacing."
+	TEMP_PATH=/tmp/diktor_tmp
+	awk "
+		BEGIN {p=1}
+		/^\\/\\/ $START/ {print;system(\"cat diktor\");p=0}
+		/^\\/\\/ $END/ {p=1}
+		p" $OUTPUT_PATH > $TEMP_PATH
+		mv $TEMP_PATH $OUTPUT_PATH
 else
-    cp prog $OUTPUT_PATH
+	echo "First install, appending"
+	(echo "// $START"; cat ./diktor; echo "// $END") >> $OUTPUT_PATH
 fi
